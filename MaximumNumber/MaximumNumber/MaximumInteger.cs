5bba5dfd0cb98e921bd60865d0ec869e4fe0f874
IndiraShahi <IndiraShahi144@gmail.com>
2021-07-01 23:48:06 +0530

    [Indira] ADDED: Extend the max method to also print the max to std out using generic method

--- MaximumNumber/MaximumNumber/MaximumInteger.cs
+++ MaximumNumber/MaximumNumber/MaximumInteger.cs
@@ -7,6 +7,11 @@ namespace MaximumNumber
     public class MaxNumber
     {
         dynamic temp;
+        /// <summary>
+        /// Method  to sort array
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
         public void MaxValue<T>(T[] array) where T : IComparable
         {
             for (int i = 0; i < array.Length - 1; i++) // For number of passes
